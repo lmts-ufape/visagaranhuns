2ced847e52a409ca42330418277e3a100ee26d3c
Danillo Bion <danilobion@gmail.com>
2020-08-21 14:22:08 -0300

    novo - tela de cadastrar empresa

---
+++ resources/views/nao_logado/cadastar_empresa.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    CADASTRAR EMPRESA (nao_logado)
+</div>
+@endsection
--- resources/views/nao_logado/home_nao_logado.blade.php
+++ resources/views/nao_logado/home_nao_logado.blade.php
@@ -3,5 +3,13 @@
 @section('content')
 <div class="container">
     PAGINA DO USUARIO COMUM (nao_logado)
+    <div class="row justify-content-center">
+        <div style="background-color:aqua;margin:0.9rem;width:250px;"><a>Cadastar Empresa</a></div>
+        <div style="background-color:beige;margin:0.9rem;width:250px;">Card_2</div>
+        <div style="background-color:blueviolet;margin:0.9rem;width:250px;">Card_3</div>
+        <div style="background-color:chocolate;margin:0.9rem;width:250px;">Card_4</div>
+
+
+    </div>
 </div>
 @endsection
