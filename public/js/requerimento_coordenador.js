af2211d6054018d1bc7a9b088871e34d529fbdcb
paulo <paulosousa1011@gmail.com>
2020-08-31 22:08:52 -0300

    Funcao js para filtro de requerimentos

--- public/js/requerimento_coordenador.js
+++ public/js/requerimento_coordenador.js
@@ -11,6 +11,23 @@ window.onload= function() {
     });
 };
 
+window.selecionarFiltro = function(){
+    //area
+    var historySelectList = $('select#idSelecionarFiltro');
+    var $opcao = $('option:selected', historySelectList).val();
+    // console.log($opcao);
+    $.ajax({
+        url:'/requerimento',
+        type:"get",
+        dataType:'json',
+        data: {"id_area": $opcao},
+        success: function(response){
+            $('tbody').html(response.table_data);
+            // document.getElementById('idArea');
+        }
+    });
+}
+
 window.mostrar = function($id){
     if(document.getElementById("cardEstabelecimento"+$id).style.display == "none"){
         document.getElementById("cardEstabelecimento"+$id).style.display = "block";
