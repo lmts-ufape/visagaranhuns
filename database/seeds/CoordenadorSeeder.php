a0428913db252152f185165bead0c4216cb489f2
paulo <paulosousa1011@gmail.com>
2020-09-18 00:38:29 -0300

    Comentando cadastro de empresas por seeder

--- database/seeds/CoordenadorSeeder.php
+++ database/seeds/CoordenadorSeeder.php
@@ -20,71 +20,71 @@ class CoordenadorSeeder extends Seeder
             'status_cadastro' => 'aprovado',
         ]);
 
-        \App\User::create([
-            'name' => 'Empresa',
-            'email' => 'empresa@teste.com',
-            'password' => Hash::make('123456'),
-            'tipo' => 'empresa',
-            'status_cadastro' => 'aprovado',
-        ]);
+        // \App\User::create([
+        //     'name' => 'Empresa',
+        //     'email' => 'empresa@teste.com',
+        //     'password' => Hash::make('123456'),
+        //     'tipo' => 'empresa',
+        //     'status_cadastro' => 'aprovado',
+        // ]);
 
-        \App\User::create([
-            'name' => 'Empresa 2',
-            'email' => 'empresa2@teste.com',
-            'password' => Hash::make('123456'),
-            'tipo' => 'empresa',
-            'status_cadastro' => 'aprovado',
-        ]);
+        // \App\User::create([
+        //     'name' => 'Empresa 2',
+        //     'email' => 'empresa2@teste.com',
+        //     'password' => Hash::make('123456'),
+        //     'tipo' => 'empresa',
+        //     'status_cadastro' => 'aprovado',
+        // ]);
 
-        \App\Empresa::create([
-            'nome' => 'Ferreira Costa',
-            'cnpjcpf' => '10325647899',
-            'status_inspecao' => 'pendente',
-            'status_cadastro' => 'pendente',
-            'tipo' => 'mei',
-            'user_id' => '2',
-        ]);
+        // \App\Empresa::create([
+        //     'nome' => 'Ferreira Costa',
+        //     'cnpjcpf' => '10325647899',
+        //     'status_inspecao' => 'pendente',
+        //     'status_cadastro' => 'pendente',
+        //     'tipo' => 'mei',
+        //     'user_id' => '2',
+        // ]);
 
-        \App\Empresa::create([
-            'nome' => 'Sobral Jóias',
-            'cnpjcpf' => '10325647899',
-            'status_inspecao' => 'pendente',
-            'status_cadastro' => 'pendente',
-            'tipo' => 'mei',
-            'user_id' => '3',
-        ]);
+        // \App\Empresa::create([
+        //     'nome' => 'Sobral Jóias',
+        //     'cnpjcpf' => '10325647899',
+        //     'status_inspecao' => 'pendente',
+        //     'status_cadastro' => 'pendente',
+        //     'tipo' => 'mei',
+        //     'user_id' => '3',
+        // ]);
         
-        \App\Endereco::create([
-            'rua' => 'Rivendell',
-            'numero' => '888',
-            'bairro' => 'Eriador',
-            'cidade' => 'Eregion',
-            'uf' => 'TM',
-            'cep' => '00000000',
-            'complemento' => 'Sudeste de Rhudaur',
-            'empresa_id' => '1',
-        ]);
+        // \App\Endereco::create([
+        //     'rua' => 'Rivendell',
+        //     'numero' => '888',
+        //     'bairro' => 'Eriador',
+        //     'cidade' => 'Eregion',
+        //     'uf' => 'TM',
+        //     'cep' => '00000000',
+        //     'complemento' => 'Sudeste de Rhudaur',
+        //     'empresa_id' => '1',
+        // ]);
 
-        \App\Endereco::create([
-            'rua' => 'Ithil',
-            'numero' => '000',
-            'bairro' => 'White Tower',
-            'cidade' => 'Minas Tirith',
-            'uf' => 'TM',
-            'cep' => '00000000',
-            'complemento' => 'Noroeste da Terra Média',
-            'empresa_id' => '2',
-        ]);
+        // \App\Endereco::create([
+        //     'rua' => 'Ithil',
+        //     'numero' => '000',
+        //     'bairro' => 'White Tower',
+        //     'cidade' => 'Minas Tirith',
+        //     'uf' => 'TM',
+        //     'cep' => '00000000',
+        //     'complemento' => 'Noroeste da Terra Média',
+        //     'empresa_id' => '2',
+        // ]);
 
-        \App\Telefone::create([
-            'telefone1' => '37626159',
-            'empresa_id' => '1',
-        ]);
+        // \App\Telefone::create([
+        //     'telefone1' => '37626159',
+        //     'empresa_id' => '1',
+        // ]);
 
-        \App\Telefone::create([
-            'telefone1' => '37956447',
-            'empresa_id' => '2',
-        ]);
+        // \App\Telefone::create([
+        //     'telefone1' => '37956447',
+        //     'empresa_id' => '2',
+        // ]);
 
         \App\User::create([
             'name' => 'inspetor',
@@ -101,10 +101,10 @@ class CoordenadorSeeder extends Seeder
             'status_cadastro' => 'aprovado',
         ]);
         
-        \App\CnaeEmpresa::create([ 'empresa_id' => '1', 'cnae_id' => '1' ]);
-        \App\CnaeEmpresa::create([ 'empresa_id' => '1', 'cnae_id' => '2' ]);
-        \App\CnaeEmpresa::create([ 'empresa_id' => '1', 'cnae_id' => '3' ]);
-        \App\CnaeEmpresa::create([ 'empresa_id' => '2', 'cnae_id' => '3' ]);
+        // \App\CnaeEmpresa::create([ 'empresa_id' => '1', 'cnae_id' => '1' ]);
+        // \App\CnaeEmpresa::create([ 'empresa_id' => '1', 'cnae_id' => '2' ]);
+        // \App\CnaeEmpresa::create([ 'empresa_id' => '1', 'cnae_id' => '3' ]);
+        // \App\CnaeEmpresa::create([ 'empresa_id' => '2', 'cnae_id' => '3' ]);
 
         
     }
