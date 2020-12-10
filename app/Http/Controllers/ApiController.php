1bdfc3ed98227b766e508f5c9deb8180761d199b
Danillo Bion <danillobion@gmail.com>
2020-12-10 20:11:07 -0300

    API - atualizar inspecoes

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -500,14 +500,13 @@ class ApiController extends Controller
                 );
                 array_push($listaDeImagens, $objFoto);
             }
-
-            $data = array(
-                'success'                   => $status,
-                'lista_inspecoes'           => $listaDeInspecoes,
-                'lista_documentos'          => $listaDeDocumentos,
-                'lista_imagens'             => $listaDeImagens,
-            );
-            echo json_encode($data);
         }
+        $data = array(
+            'success'                   => $status,
+            'lista_inspecoes'           => $listaDeInspecoes,
+            'lista_documentos'          => $listaDeDocumentos,
+            'lista_imagens'             => $listaDeImagens,
+        );
+        echo json_encode($data);
     }
 }
