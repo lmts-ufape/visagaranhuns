22504af3c90c69ad2b8c4b15d621f0851484ae7d
paulo <paulosousa1011@gmail.com>
2020-12-17 18:33:40 -0300

    Modal para exibicao de imagens no perfil do inspetor.

--- resources/views/inspetor/album_inspetor.blade.php
+++ resources/views/inspetor/album_inspetor.blade.php
@@ -85,6 +85,33 @@
                     </div>
                     </div>
                 </div>
+
+                <!-- Modal -->
+                    <div class="modal fade" id="exampleImagemInspecaoModal{{$item->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Imagem</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="form-row">
+                                    <div class="col-md-12">
+                                        <img id="imgAlbumInspecao" alt="Logo" style="padding-right:0px; width:100%"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-light" data-dismiss="modal" style="width:230px;">Fechar</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                <!--x Modal x-->
+
+
                 @endforeach
             @else
                 <div class="barraMenu" style="padding-top:6rem; padding-bottom:6rem; text-align:center; margin-bottom:3rem;">
@@ -94,29 +121,7 @@
         </div>
     </div>
 </div>
-<!-- Modal -->
-<div class="modal fade" id="exampleImagemInspecaoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Imagem</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-            <div class="form-row">
-                <div class="col-md-12">
-                    <img id="imgAlbumInspecao" alt="Logo" style="padding-right:0px; width:100%"/>
-                </div>
-            </div>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-light" data-dismiss="modal" style="width:230px;">Fechar</button>
-        </div>
-    </div>
-    </div>
-</div>
+
 <!-- Modal Deletar uma foto-->
 <div class="modal fade" id="exampleDeletarImagemInspecaoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
